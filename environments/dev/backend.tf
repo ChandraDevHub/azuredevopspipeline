79b57959f4d67994cbe0f8c5717b101492b6bcf6
Chandra Shekhar <chandrashekhar@saqlainkhandevops74gmail.onmicrosoft.com>
2025-12-25 21:46:30 +0530

    Azure pipeline and backend.tf file added

--- environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-chandra"
-    storage_account_name = "stgchandra2"
-    container_name       = "tfstate"
-    key                  = "${var.environment}.terraform.tfstate"
-  }
-}
